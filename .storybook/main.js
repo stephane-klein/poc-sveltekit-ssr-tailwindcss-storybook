543c180314937fe46d04f0a86af8664e0e1fdabd
Stéphane Klein <contact@stephane-klein.info>
2023-09-22 16:38:49 +0200

    Setup Tailwind in Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,25 @@
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  framework: {
-    name: "@storybook/sveltekit",
-    options: {}
-  },
-  docs: {
-    autodocs: "tag"
-  }
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        {
+            name: "@storybook/addon-styling",
+            options: {
+                postCss: {
+                    implementation: require.resolve('postcss'),
+                }
+            }
+        }
+    ],
+    framework: {
+        name: "@storybook/sveltekit",
+        options: {}
+    },
+    docs: {
+        autodocs: "tag"
+    }
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,26 @@
-/** @type { import('@storybook/svelte').Preview } */
+import { withThemeByClassName } from "@storybook/addon-styling";
+
+import "../src/app.css";
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/
-      }
-    }
-  }
+    parameters: {
+        actions: { argTypesRegex: "^on[A-Z].*" },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/
+            }
+        }
+    },
+
+    decorators: [
+        withThemeByClassName({
+            themes: {
+                light: "light",
+                dark: "dark"
+            },
+            defaultTheme: "light"
+        })
+    ]
 };
 
 export default preview;
